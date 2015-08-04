e645994a93057e44830547edb5fe0046332f28e2
Asheem Linaval <linaval@chickens.lan>
2015-08-03 20:09:21 -0700

    Fixed Parametric Generator script, added JSON and OPC directives.

--- openpixelcontrol/layouts/element_parametric.py
+++ openpixelcontrol/layouts/element_parametric.py
@@ -28,7 +28,7 @@ def ring(start_index, end_index, phys_theta_v, phys_sphere_radius, phys_led_spac
 	phys_cur_z = round ( math.cos(phys_theta_v), 4 )
 
 	# start populating the arc
-	for cur_element_index in range(start_index, end_index):
+	for cur_element_index in range(start_index, (end_index + 1) ):
 			# calculate x and y coordinates of 3D 
 			# phys_x, phys_y, phys_z
 			
@@ -48,7 +48,7 @@ def ring(start_index, end_index, phys_theta_v, phys_sphere_radius, phys_led_spac
 			map_cur_y = int( (math.cos(cur_element_theta) * map_ring_rad) + (map_height / 2) )
 
 			# Print gl_server JSON directive
-			print( "  {\"point\": [" + str(phys_cur_x) + ", " + str(phys_cur_y) + ", " + str(phys_cur_z) +"]},")
+			#print( "  {\"point\": [" + str(phys_cur_x) + ", " + str(phys_cur_y) + ", " + str(phys_cur_z) +"]},")
 
 			# print Processing OPC layout directive
 			print( "opc.led(" + str(cur_element_index) + ", " + str(map_cur_x) + ", " + str(map_cur_y) + ");" )
@@ -58,9 +58,24 @@ def ring(start_index, end_index, phys_theta_v, phys_sphere_radius, phys_led_spac
 
 
 # here's an example. 
-ring(start_index = 0, end_index = 24, phys_theta_v = 1.6, phys_sphere_radius = 1, phys_led_spacing = 0.123, map_ring_rad = 260, map_width = 800, map_height = 800, is_clockwise = 1, is_circle = 1, theta_h_offset = 0)
-ring(start_index = 25, end_index = 50, phys_theta_v = 0.8, phys_sphere_radius = 1, phys_led_spacing = 0.123, map_ring_rad = 150, map_width = 800, map_height = 800, is_clockwise = 0, is_circle = 0, theta_h_offset = 0)
-ring(start_index = 51, end_index = 75, phys_theta_v = 2.4, phys_sphere_radius = 1, phys_led_spacing = 0.123, map_ring_rad = 350, map_width = 800, map_height = 800, is_clockwise = 1, is_circle = 0, theta_h_offset = 0.1)
+ring(start_index = 0, end_index = 6, phys_theta_v = 0.1587, phys_sphere_radius = 0.9652, phys_led_spacing = 0.123, map_ring_rad = 50, map_width = 800, map_height = 800, is_clockwise = 1, is_circle = 1, theta_h_offset = 0)
+ring(start_index = 7, end_index = 21, phys_theta_v = 0.2870, phys_sphere_radius = 0.9652, phys_led_spacing = 0.123, map_ring_rad = 70, map_width = 800, map_height = 800, is_clockwise = 1, is_circle = 1, theta_h_offset = 0)
+ring(start_index = 22, end_index = 42, phys_theta_v = 0.4153, phys_sphere_radius = 0.9652, phys_led_spacing = 0.123, map_ring_rad = 90, map_width = 800, map_height = 800, is_clockwise = 1, is_circle = 1, theta_h_offset = 0)
+ring(start_index = 43, end_index = 69, phys_theta_v = 0.5436, phys_sphere_radius = 0.9652, phys_led_spacing = 0.123, map_ring_rad = 110, map_width = 800, map_height = 800, is_clockwise = 0, is_circle = 0, theta_h_offset = 0)
+ring(start_index = 70, end_index = 101, phys_theta_v = 0.6719, phys_sphere_radius = 0.9652, phys_led_spacing = 0.123, map_ring_rad = 130, map_width = 800, map_height = 800, is_clockwise = 1, is_circle = 0, theta_h_offset = 0)
+ring(start_index = 102, end_index = 121, phys_theta_v = 0.8002, phys_sphere_radius = 0.9652, phys_led_spacing = 0.123, map_ring_rad = 150, map_width = 800, map_height = 800, is_clockwise = 0, is_circle = 0, theta_h_offset = 0)
+ring(start_index = 122, end_index = 142, phys_theta_v = 0.9285, phys_sphere_radius = 0.9652, phys_led_spacing = 0.123, map_ring_rad = 170, map_width = 800, map_height = 800, is_clockwise = 1, is_circle = 0, theta_h_offset = 0)
+ring(start_index = 143, end_index = 164, phys_theta_v = 1.0568, phys_sphere_radius = 0.9652, phys_led_spacing = 0.123, map_ring_rad = 190, map_width = 800, map_height = 800, is_clockwise = 0, is_circle = 0, theta_h_offset = 0)
+ring(start_index = 165, end_index = 187, phys_theta_v = 1.1851, phys_sphere_radius = 0.9652, phys_led_spacing = 0.123, map_ring_rad = 210, map_width = 800, map_height = 800, is_clockwise = 1, is_circle = 0, theta_h_offset = 0)
+ring(start_index = 188, end_index = 211, phys_theta_v = 1.3134, phys_sphere_radius = 0.9652, phys_led_spacing = 0.123, map_ring_rad = 230, map_width = 800, map_height = 800, is_clockwise = 0, is_circle = 0, theta_h_offset = 0)
+ring(start_index = 212, end_index = 237, phys_theta_v = 1.4417, phys_sphere_radius = 0.9652, phys_led_spacing = 0.123, map_ring_rad = 250, map_width = 800, map_height = 800, is_clockwise = 1, is_circle = 0, theta_h_offset = 0)
 
 
 
+#ring(start_index = 0, end_index = 24, phys_theta_v = 1.4417, phys_sphere_radius = 0.9652, phys_led_spacing = 0.123, map_ring_rad = 250, map_width = 800, map_height = 800, is_clockwise = 0, is_circle = 0, theta_h_offset = 0)
+#ring(start_index = 25, end_index = 48, phys_theta_v = 1.3134, phys_sphere_radius = 0.9652, phys_led_spacing = 0.123, map_ring_rad = 230, map_width = 800, map_height = 800, is_clockwise = 1, is_circle = 0, theta_h_offset = 0)
+#ring(start_index = 49, end_index = 70, phys_theta_v = 1.1851, phys_sphere_radius = 0.9652, phys_led_spacing = 0.123, map_ring_rad = 210, map_width = 800, map_height = 800, is_clockwise = 1, is_circle = 0, theta_h_offset = 0)
+#ring(start_index = 71, end_index = 91, phys_theta_v = 1.0568, phys_sphere_radius = 0.9652, phys_led_spacing = 0.123, map_ring_rad = 190, map_width = 800, map_height = 800, is_clockwise = 0, is_circle = 0, theta_h_offset = 0)
+
+#ring(start_index = 92, end_index = 92, phys_theta_v = 0.9285, phys_sphere_radius = 0.9652, phys_led_spacing = 0.123, map_ring_rad = 170, map_width = 800, map_height = 800, is_clockwise = 1, is_circle = 0, theta_h_offset = 0)
+#ring(start_index = 138, end_index = 158, phys_theta_v = 1.0568, phys_sphere_radius = 0.9652, phys_led_spacing = 0.123, map_ring_rad = 190, map_width = 800, map_height = 800, is_clockwise = 0, is_circle = 0, theta_h_offset = 0)
